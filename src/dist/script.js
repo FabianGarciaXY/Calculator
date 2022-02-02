5360bb16e59186b536e0b17d9460bd34b9081d14
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-02-02 17:56:44 -0600

    Added add button functionality

--- src/dist/script.js
+++ src/dist/script.js
@@ -18,8 +18,13 @@ const divide = function (a, b) {
     const result = a / b;
     return parseFloat(result.toFixed(1));
 };
-// Digits EventListeners
-let displayValue = "";
+// Main value on screen
+let displayValue1 = '';
+let displayValue2 = '';
+let value1;
+let value2;
+let result;
+// Cursor blink
 const cursor = document.querySelector('#cursor');
 cursor.textContent = '';
 setInterval(() => {
@@ -30,25 +35,77 @@ setInterval(() => {
         cursor.textContent = '';
     }
 }, 600);
-// FIX IT ******
-function printOnScreen(e) {
-    const screen = document.querySelector('#input');
-    displayValue = displayValue.concat(e.target.textContent);
-    screen.textContent = displayValue;
-}
-const clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', clear);
-function clear() {
-    displayValue = "";
-    const screen = document.querySelector('.inputs');
-    screen.textContent = displayValue;
-}
-// Creating digits Elements
+// Function to populate screen(#input) when clicking a digit & creating digits Elements
 const digits = document.querySelector('.digits');
 for (let digit = 1; digit <= 9; digit++) {
     const newDigit = document.createElement('button');
     newDigit.classList.add('digit');
     newDigit.textContent = digit.toString();
-    newDigit.addEventListener('click', printOnScreen);
+    newDigit.addEventListener('click', printOnScreen1);
     digits.appendChild(newDigit);
 }
+function printOnScreen1(e) {
+    const firstInput = document.querySelector('#first-input');
+    displayValue1 = displayValue1.concat(e.target.textContent);
+    firstInput.textContent = displayValue1;
+}
+function printOnScreen2(e) {
+    const secondInput = document.querySelector('#second-input');
+    displayValue2 = displayValue2.concat(e.target.textContent);
+    secondInput.textContent = displayValue2;
+}
+// Operations listeners 
+/* Add operation */
+const addButton = document.querySelector('#add');
+const operation = document.querySelector('#operation');
+const resultDisplay = document.querySelector('#result');
+addButton.addEventListener('click', () => {
+    value1 = parseInt(displayValue1);
+    value2 = parseInt(displayValue2);
+    if (!value1) {
+        alert('Please input a number first');
+        return;
+    }
+    else if (value2) {
+        alert('You can only add two numbers');
+        return;
+    }
+    operation.textContent = '+';
+    const gettinDigits = document.querySelectorAll('.digit');
+    gettinDigits.forEach(digit => {
+        digit.removeEventListener('click', printOnScreen1);
+        digit.addEventListener('click', printOnScreen2);
+    });
+    console.log(addButton, value1, displayValue1);
+});
+// Result operation
+const equalsButton = document.querySelector('#equals-button');
+equalsButton.addEventListener('click', () => {
+    value1 = parseFloat(displayValue1);
+    value2 = parseFloat(displayValue2);
+    console.log(value1, value2);
+    console.log(operate(add, value1, value2));
+    result = operate(add, value1, value2);
+    resultDisplay.textContent = result.toString();
+});
+// Clear button
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clear);
+function clear() {
+    displayValue1 = '';
+    displayValue2 = '';
+    operation.textContent = '';
+    const screen1 = document.querySelector('#first-input');
+    const screen2 = document.querySelector('#second-input');
+    screen1.textContent = displayValue1;
+    screen2.textContent = displayValue2;
+    resultDisplay.textContent = "";
+    const gettinDigits = document.querySelectorAll('.digit');
+    gettinDigits.forEach(digit => {
+        digit.addEventListener('click', printOnScreen1);
+    });
+    gettinDigits.forEach(digit => {
+        digit.removeEventListener('click', printOnScreen2);
+    });
+}
+;
--- src/script.ts
+++ src/script.ts
@@ -28,12 +28,16 @@ const divide = function(a: number, b:number) {
 
 
 
+// Main value on screen
+let displayValue1: string = '';
+let displayValue2: string = ''
+let value1: number;
+let value2: number;
+let result: number;
 
 
-// Digits EventListeners
-let displayValue: string = "";
 
-
+// Cursor blink
 const cursor:HTMLElement = document.querySelector('#cursor');
 cursor.textContent = '';
 setInterval( () => {
@@ -46,36 +50,100 @@ setInterval( () => {
 
 
 
-// FIX IT ******
-function printOnScreen(e): void {
+// Function to populate screen(#input) when clicking a digit & creating digits Elements
 
-    const screen: HTMLElement = document.querySelector('#input');
-    displayValue = displayValue.concat(e.target.textContent);
-
-    screen.textContent = displayValue;
-}
-
-
-
-const clearButton: HTMLElement = document.querySelector('#clear-button');
-clearButton.addEventListener('click', clear);
-function clear() {
-    displayValue = "";
-    const screen: HTMLElement = document.querySelector('.inputs');
-    screen.textContent = displayValue;
-}
-
-
-
-// Creating digits Elements
 const digits: HTMLElement = document.querySelector('.digits');
+
 for (let digit: number = 1 ; digit <= 9; digit ++) {
     
     const newDigit: HTMLElement = document.createElement('button');
     newDigit.classList.add('digit');
     newDigit.textContent = digit.toString();
 
-    newDigit.addEventListener('click', printOnScreen)
-
+    newDigit.addEventListener('click', printOnScreen1)
     digits.appendChild(newDigit);
 }
+
+function printOnScreen1(e): void {
+    const firstInput: HTMLElement = document.querySelector('#first-input');
+    displayValue1 = displayValue1.concat(e.target.textContent);
+    firstInput.textContent = displayValue1;
+}
+function printOnScreen2(e): void {
+    const secondInput: HTMLElement = document.querySelector('#second-input');
+    displayValue2 = displayValue2.concat(e.target.textContent);
+    secondInput.textContent = displayValue2;
+}
+
+
+
+// Operations listeners 
+/* Add operation */
+const addButton: HTMLElement = document.querySelector('#add');
+const operation: HTMLElement = document.querySelector('#operation');
+const resultDisplay: HTMLElement = document.querySelector('#result');
+
+
+addButton.addEventListener('click', () => {
+
+    value1 = parseInt(displayValue1);
+    value2 = parseInt(displayValue2);
+
+    if (!value1) {
+        alert('Please input a number first');
+        return;
+    } else if(value2) {
+        alert('You can only add two numbers')
+        return;
+    }
+
+    operation.textContent = '+';
+
+    const gettinDigits = document.querySelectorAll('.digit');
+    gettinDigits.forEach( digit => {
+        digit.removeEventListener('click', printOnScreen1);
+        digit.addEventListener('click', printOnScreen2);
+    })
+
+    console.log(addButton, value1, displayValue1);
+})
+
+
+// Result operation
+const equalsButton: HTMLElement = document.querySelector('#equals-button');
+equalsButton.addEventListener('click', () => {
+    value1 = parseFloat(displayValue1);
+    value2 = parseFloat(displayValue2);
+
+    console.log(value1, value2);
+    console.log(operate(add, value1, value2));
+    result = operate(add, value1, value2);
+    resultDisplay.textContent = result.toString();
+})
+
+
+
+// Clear button
+const clearButton: HTMLElement = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clear);
+function clear() {
+    displayValue1 = '';
+    displayValue2 = '';
+    operation.textContent = '';
+
+    const screen1: HTMLElement = document.querySelector('#first-input');
+    const screen2: HTMLElement = document.querySelector('#second-input');
+
+
+    screen1.textContent = displayValue1;
+    screen2.textContent = displayValue2;
+    resultDisplay.textContent = "";
+
+    const gettinDigits = document.querySelectorAll('.digit');
+    gettinDigits.forEach( digit => {
+        digit.addEventListener('click', printOnScreen1);
+    });
+    gettinDigits.forEach( digit => {
+        digit.removeEventListener('click', printOnScreen2);
+    });
+};
